18dc78a944164f291aefa5cec48dab5025a9450c
Adam Berger <thedude112286@gmail.com>
2011-07-01 12:40:32 -0400

    Added links to routes

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 var Routes = function() {
     this._routes = {};
-}
+};
 
 Routes.prototype.addRoute = function(name, url) {
     this._routes[name] = url;
@@ -8,10 +8,15 @@ Routes.prototype.addRoute = function(name, url) {
     this.__defineGetter__(name, function() {
         return url;
     });
-}
+};
 
 Routes.prototype.getRoute = function(name) {
     return this._routes[name];
-}
+};
+
+Routes.prototype.exposeHelpers = function(app) {
+    var self = this;
+    app.helpers({'linkTo': function(name) { return self[name]; }});
+};
 
 module.exports = new Routes();
