c8004f562e7538772ac9b0ada22cc678a0fa7702
Adam Berger <adam@ezbds.com>
2012-06-24 23:30:16 -0400

    Added param fill in and allForPrefix

--- routes.js
+++ routes.js
@@ -10,8 +10,38 @@ Routes.prototype.addRoute = function(name, url) {
     });
 };
 
-Routes.prototype.getRoute = function(name) {
-    return this._routes[name];
+Routes.prototype.getRoute = function(name, values, qsParams) {
+    var r = this._routes[name];
+    if ( arguments.length === 1 ) {
+        return r;
+    }
+
+    values = values || {};
+    qsParams = qsParams || {};
+
+    var withValues = Object.keys(values).reduce(function(key, r) {
+            return r.replace(':' + key, values[key]);
+        }, r),
+        andedQs = Object.keys(qsParams).map(function(k) { return encodeURIComponent(k) + '=' + encodeURIComponent(qsParams[k]); })
+                        .reduce(function(a, b) { return a + '&' + b; });
+        withQs = withValues + (withValues.indexOf('?') === -1 ? '?' : '') + andedQs;
+
+    return withQs;
+};
+
+Routes.prototype.allForPrefix = function(prefix) {
+    var self = this,
+        rs = {},
+        regex = new RegExp('^' + prefix),
+        newRoutes = new Routes();
+    Object.keys(this._routes).forEach(function(r) {
+        if ( regex.exec(self._routes[r]) ) {
+            rs[r] = self._routes[r];
+        }
+    });
+
+    newRoutes._routes = rs;
+    return newRoutes;
 };
 
 Routes.prototype.exposeHelpers = function(app) {
